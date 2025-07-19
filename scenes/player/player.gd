1e450dc66352b20205fc82c0cb57d89955e9bf9b
TonyinFL <tony@nettisconsulting.com>
2025-07-18 21:28:05 -0400

    Add support for player input

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,13 +1,30 @@
 class_name Player extends Area2D
 
 const GROUP_NAME: String = "Player"
+const PLAYER_SPEED: float = 250.0  # Pixels per second
+const BULLET_SPEED: float = 300.0  # Pixels per second
+const BULLET_DIRECTION: Vector2 = Vector2.UP
+
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var shield: Shield = $Shield
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var _viewport: Rect2 
+var _upper_left: Vector2
+var _lower_right: Vector2
+var _margin: Vector2
 
 
 func _ready() -> void:
-	pass
+	set_limits()
+
+
+func set_limits() -> void:
+	_margin = sprite_2d.get_rect().size * sprite_2d.scale * .5
+	_viewport = get_viewport_rect()
+	_lower_right = Vector2(_viewport.end.x - _margin.x, _viewport.end.y - _margin.y)
+	_upper_left = Vector2(_margin.x, _margin.y)  # Assumes viewport starts at 0,0
 
 
 func _enter_tree() -> void:
@@ -15,9 +32,37 @@ func _enter_tree() -> void:
 
 
 func _process(delta: float) -> void:
-	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
-	position += direction * 400.0 * delta		
+	global_position += get_input() * PLAYER_SPEED * delta
+	global_position = global_position.clamp(_upper_left, _lower_right)
+	
+	
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 
+
+func get_input() -> Vector2:
+	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+	# Note: You can build a Vector2 using two normalized Input.get_axis calls,
+	# like Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down")).
+	# However, get_vector() automatically normalizes the result and applies
+	# a deadzone to filter small analog joystick noise.
+	
+	if not is_zero_approx(direction.x):
+		animation_player.play("turn")
+		sprite_2d.flip_h = direction.x > 0
+	else:
+		animation_player.play("fly")
+		
+	return direction
+		
+
+func shoot() -> void:
+	SignalHub.emit_on_create_bullet(
+		BulletBase.BulletType.Player,
+		global_position, 
+		BULLET_DIRECTION,
+		BULLET_SPEED
+	)
 
 func _on_area_entered(area: Area2D) -> void:
 	if area is PowerUp:
