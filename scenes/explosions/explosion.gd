9a4ee99d50c2b47dc31be7663c0185c277d1280f
TonyinFL <tony@nettisconsulting.com>
2025-07-23 21:42:49 -0400

    Improve sound effects

--- scenes/explosions/explosion.gd
+++ scenes/explosions/explosion.gd
@@ -2,7 +2,10 @@ class_name Explosion extends Node2D
 
 const BOOM: String = "boom"
 const EXPLODE: String = "explode"
+const EXPLOSION_01: AudioStream = preload("res://assets/sounds/explosions/explosion_01.wav")
+const EXPLOSION_05: AudioStream = preload("res://assets/sounds/explosions/explosion_05.wav")
 
+@onready var sound: AudioStreamPlayer = $Sound
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 var _animation_name: String = BOOM
@@ -10,6 +13,15 @@ var _animation_name: String = BOOM
 func _ready() -> void:
 	animated_sprite_2d.animation = _animation_name
 	animated_sprite_2d.play()
+	match _animation_name:
+		BOOM:
+			sound.stream = EXPLOSION_01
+		EXPLODE:
+			sound.stream = EXPLOSION_05
+		_:
+			sound.stream = EXPLOSION_01  # Default sound
+	sound.play()
+	
 
 func setup(animation_name: String) -> void:
 	_animation_name = animation_name
